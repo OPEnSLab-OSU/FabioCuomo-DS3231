52a8f10a43f65104f55a1c27c1038e865117a4c9
goertzel <goertzel@oregonstate.edu>
2019-03-15 14:38:45 -0700

    Indentation fixes

--- OPEnS_RTC.h
+++ OPEnS_RTC.h
@@ -14,12 +14,11 @@ class TimeSpan;
 
 
 //Begin PCF8523 definitions
-
-#define PCF8523_ADDRESS              	0x68
-#define PCF8523_CLKOUTCONTROL        	0x0F
-#define PCF8523_CONTROL_1       	0x00
-#define PCF8523_CONTROL_2		0x01
-#define PCF8523_CONTROL_3            	0x02
+#define PCF8523_ADDRESS				0x68
+#define PCF8523_CLKOUTCONTROL		0x0F
+#define PCF8523_CONTROL_1			0x00
+#define PCF8523_CONTROL_2			0x01
+#define PCF8523_CONTROL_3			0x02
 
 #define PCF8523_SECONDS				0x03
 #define PCF8523_MINUTES				0x04
@@ -119,7 +118,7 @@ class TimeSpan;
 #define A2M3 7
 #define A2M4 7
 
-//DS3232 Register Addresses
+//DS3231 Register Addresses
 #define ALM1_SECONDS 0x07
 #define ALM1_MINUTES 0x08
 #define ALM1_HOURS 0x09
@@ -136,87 +135,87 @@ const uint8_t RTC_CLKOUT_DISABLED = ((1<<3) | (1<<4) | (1<<5));
 // Simple general-purpose date/time class (no TZ / DST / leap second handling!)
 class DateTime {
 public:
-    DateTime (uint32_t t =0);
-    DateTime (uint16_t year, uint8_t month, uint8_t day,
-                uint8_t hour =0, uint8_t min =0, uint8_t sec =0);
-    DateTime (const DateTime& copy);
-    DateTime (const char* date, const char* time);
-    DateTime (const __FlashStringHelper* date, const __FlashStringHelper* time);
-    uint16_t year() const       { return 2000 + yOff; }
-    uint8_t month() const       { return m; }
-    uint8_t day() const         { return d; }
-    uint8_t hour() const        { return hh; }
-    uint8_t minute() const      { return mm; }
-    uint8_t second() const      { return ss; }
-    uint8_t dayOfTheWeek() const;
+	DateTime (uint32_t t =0);
+	DateTime (uint16_t year, uint8_t month, uint8_t day,
+				uint8_t hour =0, uint8_t min =0, uint8_t sec =0);
+	DateTime (const DateTime& copy);
+	DateTime (const char* date, const char* time);
+	DateTime (const __FlashStringHelper* date, const __FlashStringHelper* time);
+	uint16_t year() const       { return 2000 + yOff; }
+	uint8_t month() const       { return m; }
+	uint8_t day() const         { return d; }
+	uint8_t hour() const        { return hh; }
+	uint8_t minute() const      { return mm; }
+	uint8_t second() const      { return ss; }
+	uint8_t dayOfTheWeek() const;
 
-    // 32-bit times as seconds since 1/1/2000
-    long secondstime() const;   
-    // 32-bit times as seconds since 1/1/1970
-    uint32_t unixtime(void) const;
+	// 32-bit times as seconds since 1/1/2000
+	long secondstime() const;   
+	// 32-bit times as seconds since 1/1/1970
+	uint32_t unixtime(void) const;
 
-    DateTime operator+(const TimeSpan& span);
-    DateTime operator-(const TimeSpan& span);
-    TimeSpan operator-(const DateTime& right);
+	DateTime operator+(const TimeSpan& span);
+	DateTime operator-(const TimeSpan& span);
+	TimeSpan operator-(const DateTime& right);
 
 protected:
-    uint8_t yOff, m, d, hh, mm, ss;
+	uint8_t yOff, m, d, hh, mm, ss;
 };
 
 // Timespan which can represent changes in time with seconds accuracy.
 class TimeSpan {
 public:
-    TimeSpan (int32_t seconds = 0);
-    TimeSpan (int16_t days, int8_t hours, int8_t minutes, int8_t seconds);
-    TimeSpan (const TimeSpan& copy);
-    int16_t days() const         { return _seconds / 86400L; }
-    int8_t  hours() const        { return _seconds / 3600 % 24; }
-    int8_t  minutes() const      { return _seconds / 60 % 60; }
-    int8_t  seconds() const      { return _seconds % 60; }
-    int32_t totalseconds() const { return _seconds; }
+	TimeSpan (int32_t seconds = 0);
+	TimeSpan (int16_t days, int8_t hours, int8_t minutes, int8_t seconds);
+	TimeSpan (const TimeSpan& copy);
+	int16_t days() const         { return _seconds / 86400L; }
+	int8_t  hours() const        { return _seconds / 3600 % 24; }
+	int8_t  minutes() const      { return _seconds / 60 % 60; }
+	int8_t  seconds() const      { return _seconds % 60; }
+	int32_t totalseconds() const { return _seconds; }
 
-    TimeSpan operator+(const TimeSpan& right);
-    TimeSpan operator-(const TimeSpan& right);
+	TimeSpan operator+(const TimeSpan& right);
+	TimeSpan operator-(const TimeSpan& right);
 
 protected:
-    int32_t _seconds;
+	int32_t _seconds;
 };
 
 class PCF8523{
 
 	public:
-    static uint8_t begin(void);
-    static void adjust(const DateTime& dt);
-    boolean initialized(void);
-    uint8_t isrunning(void);
-    static DateTime now();
-    uint8_t read_reg(uint8_t address);
-    void read_reg(uint8_t* buf, uint8_t size, uint8_t address);
-    void write_reg(uint8_t address, uint8_t data);
-    void write_reg(uint8_t address, uint8_t* buf, uint8_t size);
-    void set_alarm(uint8_t day_alarm, uint8_t hour_alarm,uint8_t minute_alarm ) ;
-    void set_alarm(uint8_t hour_alarm,uint8_t minute_alarm );
-    void get_alarm(uint8_t* buf);
-    void reset();
-    uint8_t clear_rtc_interrupt_flags(); 
-    void stop_32768_clkout();
-    void start_counter_1(uint8_t value);
+	static uint8_t begin(void);
+	static void adjust(const DateTime& dt);
+	boolean initialized(void);
+	uint8_t isrunning(void);
+	static DateTime now();
+	uint8_t read_reg(uint8_t address);
+	void read_reg(uint8_t* buf, uint8_t size, uint8_t address);
+	void write_reg(uint8_t address, uint8_t data);
+	void write_reg(uint8_t address, uint8_t* buf, uint8_t size);
+	void set_alarm(uint8_t day_alarm, uint8_t hour_alarm,uint8_t minute_alarm ) ;
+	void set_alarm(uint8_t hour_alarm,uint8_t minute_alarm );
+	void get_alarm(uint8_t* buf);
+	void reset();
+	uint8_t clear_rtc_interrupt_flags(); 
+	void stop_32768_clkout();
+	void start_counter_1(uint8_t value);
 };
 
 enum Ds1307SqwPinMode { OFF = 0x00, ON = 0x80, SquareWave1HZ = 0x10, SquareWave4kHz = 0x11, SquareWave8kHz = 0x12, SquareWave32kHz = 0x13 };
 
 class RTC_DS1307 {
 public:
-    boolean begin(void);
-    static void adjust(const DateTime& dt);
-    uint8_t isrunning(void);
-    static DateTime now();
-    static Ds1307SqwPinMode readSqwPinMode();
-    static void writeSqwPinMode(Ds1307SqwPinMode mode);
-    uint8_t readnvram(uint8_t address);
-    void readnvram(uint8_t* buf, uint8_t size, uint8_t address);
-    void writenvram(uint8_t address, uint8_t data);
-    void writenvram(uint8_t address, uint8_t* buf, uint8_t size);
+	boolean begin(void);
+	static void adjust(const DateTime& dt);
+	uint8_t isrunning(void);
+	static DateTime now();
+	static Ds1307SqwPinMode readSqwPinMode();
+	static void writeSqwPinMode(Ds1307SqwPinMode mode);
+	uint8_t readnvram(uint8_t address);
+	void readnvram(uint8_t* buf, uint8_t size, uint8_t address);
+	void writenvram(uint8_t address, uint8_t data);
+	void writenvram(uint8_t address, uint8_t* buf, uint8_t size);
 };
 
 // RTC based on the DS3231 chip connected via I2C and the Wire library
@@ -224,39 +223,39 @@ enum Ds3231SqwPinMode { DS3231_OFF = 0x01, DS3231_SquareWave1Hz = 0x00, DS3231_S
 
 //Alarm masks
 enum Ds3231_ALARM_TYPES_t {
-    ALM1_EVERY_SECOND = 0x0F,
-    ALM1_MATCH_SECONDS = 0x0E,
-    ALM1_MATCH_MINUTES = 0x0C,     //match minutes *and* seconds
-    ALM1_MATCH_HOURS = 0x08,       //match hours *and* minutes, seconds
-    ALM1_MATCH_DATE = 0x00,        //match date *and* hours, minutes, seconds
-    ALM1_MATCH_DAY = 0x10,         //match day *and* hours, minutes, seconds
+	ALM1_EVERY_SECOND = 0x0F,
+	ALM1_MATCH_SECONDS = 0x0E,
+	ALM1_MATCH_MINUTES = 0x0C,     //match minutes *and* seconds
+	ALM1_MATCH_HOURS = 0x08,       //match hours *and* minutes, seconds
+	ALM1_MATCH_DATE = 0x00,        //match date *and* hours, minutes, seconds
+	ALM1_MATCH_DAY = 0x10,         //match day *and* hours, minutes, seconds
 
-    ALM2_EVERY_MINUTE = 0x8E,
-    ALM2_MATCH_MINUTES = 0x8C,     //match minutes
-    ALM2_MATCH_HOURS = 0x88,       //match hours *and* minutes
-    ALM2_MATCH_DATE = 0x80,        //match date *and* hours, minutes
-    ALM2_MATCH_DAY = 0x90,         //match day *and* hours, minutes
+	ALM2_EVERY_MINUTE = 0x8E,
+	ALM2_MATCH_MINUTES = 0x8C,     //match minutes
+	ALM2_MATCH_HOURS = 0x88,       //match hours *and* minutes
+	ALM2_MATCH_DATE = 0x80,        //match date *and* hours, minutes
+	ALM2_MATCH_DAY = 0x90,         //match day *and* hours, minutes
 };
 
 class RTC_DS3231 {
 public:
-    boolean begin(void);
-    static void adjust(const DateTime& dt);
-    bool initialized(void);
-    bool lostPower(void);
-    static DateTime now();
-    static Ds3231SqwPinMode readSqwPinMode();
-    static void writeSqwPinMode(Ds3231SqwPinMode mode);
-    float getTemp();
-    void forceConversion(void);
-    void setAlarm(Ds3231_ALARM_TYPES_t alarmType, byte seconds, byte minutes, byte hours, byte daydate);
-    void setAlarm(Ds3231_ALARM_TYPES_t alarmType, byte minutes, byte hours, byte daydate);
-    void armAlarm(byte alarmNumber, bool armed);
-    void alarmInterrupt(byte alarmNumber, bool alarmEnabled);
-    bool isArmed(byte alarmNumber);
-    void clearAlarm(byte alarmNumber);
-    void write(byte addr, byte value);
-    byte read(byte addr);
+	boolean begin(void);
+	static void adjust(const DateTime& dt);
+	// bool initialized(void);
+	bool lostPower(void);
+	static DateTime now();
+	static Ds3231SqwPinMode readSqwPinMode();
+	static void writeSqwPinMode(Ds3231SqwPinMode mode);
+	float getTemp();
+	void forceConversion(void);
+	void setAlarm(Ds3231_ALARM_TYPES_t alarmType, byte seconds, byte minutes, byte hours, byte daydate);
+	void setAlarm(Ds3231_ALARM_TYPES_t alarmType, byte minutes, byte hours, byte daydate);
+	void armAlarm(byte alarmNumber, bool armed);
+	void alarmInterrupt(byte alarmNumber, bool alarmEnabled);
+	bool isArmed(byte alarmNumber);
+	void clearAlarm(byte alarmNumber);
+	void write(byte addr, byte value);
+	byte read(byte addr);
 };
 
 // RTC based on the PCF8523 chip connected via I2C and the Wire library
@@ -264,25 +263,25 @@ enum Pcf8523SqwPinMode { PCF8523_OFF = 7, PCF8523_SquareWave1HZ = 6, PCF8523_Squ
 
 class RTC_PCF8523 {
 public:
-    boolean begin(void);
-    void adjust(const DateTime& dt);
-    boolean initialized(void);
-    static DateTime now();
+	boolean begin(void);
+	void adjust(const DateTime& dt);
+	boolean initialized(void);
+	static DateTime now();
 
-    Pcf8523SqwPinMode readSqwPinMode();
-    void writeSqwPinMode(Pcf8523SqwPinMode mode);
+	Pcf8523SqwPinMode readSqwPinMode();
+	void writeSqwPinMode(Pcf8523SqwPinMode mode);
 };
 
 // RTC using the internal millis() clock, has to be initialized before use
 // NOTE: this clock won't be correct once the millis() timer rolls over (>49d?)
 class RTC_Millis {
 public:
-    static void begin(const DateTime& dt) { adjust(dt); }
-    static void adjust(const DateTime& dt);
-    static DateTime now();
+	static void begin(const DateTime& dt) { adjust(dt); }
+	static void adjust(const DateTime& dt);
+	static DateTime now();
 
 protected:
-    static long offset;
+	static long offset;
 };
 
 #endif // _RTCLIB_H_
